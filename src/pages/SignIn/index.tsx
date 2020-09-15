29847bb2e732c7821bd7178130c815aa8e2413f5
Antony Lemos <lemosantony@gmail.com>
2020-09-15 10:59:29 -0300

    🚧 chore: create button and input components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
@@ -9,6 +12,12 @@ const SignIn: React.FC = () => {
       <View>
         <Title>Faça seu logon</Title>
       </View>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px 150px;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
